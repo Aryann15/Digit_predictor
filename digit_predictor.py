0fdc80df296e2805e6b8c8c9963639f4e9ae2088
Aryann15 <rajlysm15@gmail.com>
2023-06-30 00:19:34 +0530

    dense layer

--- digit_predictor.py
+++ digit_predictor.py
@@ -12,3 +12,7 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model= tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(units=128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10,activation=tf.nn.softmax))
+
